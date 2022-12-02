f2dbcace4e6a0ab5474d4f4e16ec6f6b8e6f9d21
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-02 16:52:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,47 @@
+export const item = {
+    "id": "fa432026-872a-44a9-9dfe-b0c7bb3c49d5",
+    "home_carousel": [
+        {
+            "id": "f242efe0-e437-4f2f-a5f3-94994d946756",
+            "video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+        },
+        {
+            "id": "83ad5c5b-44f7-4968-b0b4-45209ec2a947",
+            "video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+        },
+        {
+            "id": "eadaa052-0948-498a-82e2-084cadbaa645",
+            "video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+        },
+        {
+            "id": "66222b89-9d20-4e4b-9c93-540213a2ca56",
+            "video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+        },
+        {
+            "id": "7626918c-3296-42e8-b827-872d6499c154",
+            "video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png"
+        }
+    ],
+    "continue_videos": [
+        {
+            "id": "f351db8d-8255-4df9-825c-2a8a725fe2f0",
+            "continue_video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+        },
+        {
+            "id": "89678e2b-c5e0-4e0b-9272-65d0d60bb0d7",
+            "continue_video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+        },
+        {
+            "id": "60d7a1f9-a242-4d6e-a528-dc8e6351ea8f",
+            "continue_video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+        },
+        {
+            "id": "16a89f47-e825-4801-a680-1e9c0e2e76a9",
+            "continue_video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+        },
+        {
+            "id": "82e7466e-f539-4c18-b8a5-227c45b0b279",
+            "continue_video_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+        }
+    ]
+}
